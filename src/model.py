943660e80e559e5be55169828db67a5613629908
jacob <jacob.l280904@gmail.com>
2025-12-07 19:56:15 -0800

    update model

--- src/model.py
+++ src/model.py
@@ -6,34 +6,34 @@ class SimpleCNN(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
 
+        # Feature extractor
         self.features = nn.Sequential(
             nn.Conv2d(3, 32, 3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(2),  # 128 -> 64
+            nn.MaxPool2d(2),  # /2
 
             nn.Conv2d(32, 64, 3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(2),  # 64 -> 32
+            nn.MaxPool2d(2),  # /4
 
             nn.Conv2d(64, 128, 3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(2),  # 32 -> 16
+            nn.MaxPool2d(2),  # /8
 
-            # New block
             nn.Conv2d(128, 256, 3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(2),  # 16 -> 8
+            nn.MaxPool2d(2),  # /16
         )
 
-        # Output feature map = 256 channels × 8 × 8 = 16384
-        # Deeper classifier + stronger dropout ***
+        # Classifier
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(256 * 8 * 8, 512),
+
+            nn.LazyLinear(512),   #auto-infers the correct input size
             nn.ReLU(),
             nn.Dropout(0.4),
 
@@ -48,7 +48,6 @@ class SimpleCNN(nn.Module):
         x = self.features(x)
         x = self.classifier(x)
         return x
-
 
 def build_model(num_classes):
     return SimpleCNN(num_classes)
